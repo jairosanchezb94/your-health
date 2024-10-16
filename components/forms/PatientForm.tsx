085d12dbe625b67d42cdb6e59a3a5c67abb7249e
jairosanchezb94 <jairosanchezb5@gmail.com>
2024-10-16 16:06:34 +0200

    adding validations

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,11 +1,14 @@
 "use client"
- 
+// minuto 50:35 youtube
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import { SubmitButton } from "./SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldTypes {
   INPUT = "input",
@@ -17,23 +20,40 @@ export enum FormFieldTypes {
   SKELETON = "skeleton",
 }
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
- 
 const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = {
+      //   name,
+      //   email,
+      //   phone
+      // }
+      // const user = await createUser(userData);
+
+      // if (user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
   }
+
+
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
@@ -66,7 +86,7 @@ const PatientForm = () => {
         label="Phone Number"
         placeholder="Enter your phone number"
       />
-      <Button type="submit">Submit</Button>
+      <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
     </form>
   </Form>
   )
