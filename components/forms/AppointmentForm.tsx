7f75cb2857d9b85a7a95e2f6f81275a8974aba79
jairosanchezb94 <jairosanchezb5@gmail.com>
2024-11-18 12:08:18 +0100

    added and implemented administrator screen

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -13,15 +13,18 @@ import { FormFieldTypes } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
  
 const AppointmentForm = ({
-    userId, patientId, type
+    userId, patientId, type, appointment, setOpen
 }: {
     userId: string;
     patientId: string;
     type: "create" | "schedule" | "cancel";
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void;
 }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -31,11 +34,11 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-        primaryPhysician: "",
-        schedule: new Date(),
-        reason: "",
-        note: "",
-        cancellationReason: "",
+        primaryPhysician: appointment ? appointment.primaryPhysician : "",
+        schedule: appointment ? new Date(appointment?.schedule) : new Date(Date.now()),
+        reason: appointment ? appointment.reason : "",
+        note: appointment?.note || "",
+        cancellationReason: appointment?.cancellationReason || "",
     },
   })
  
@@ -67,12 +70,31 @@ const AppointmentForm = ({
               note: values.note,
             }
             
-            const appointment = await createAppointment(appointmentData);
+            const newAppointment = await createAppointment(appointmentData);
 
-            if(appointment) {
+            if(newAppointment) {
                 form.reset();
-                router.push( `/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`)
+                router.push( `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`)
             }
+        } else {
+          const appointmentToUpdate = {
+            userId,
+            appointmentId: appointment?.$id!,
+            appointment: {
+              primaryPhysician: values?.primaryPhysician,
+              schedule: new Date(values?.schedule),
+              status: status as Status,
+              cancellationReason: values?.cancellationReason,
+            },
+            type,
+          };
+  
+          const updatedAppointment = await updateAppointment(appointmentToUpdate);
+  
+          if (updatedAppointment) {
+            setOpen && setOpen(false);
+            form.reset();
+          }
         }
 
 
@@ -100,10 +122,10 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-      <section className="mb-12 space-y-4">
+      {type === "create" && <section className="mb-12 space-y-4">
         <h1 className="header">New Appointment</h1>
         <p className="text-dark-700">Request a new appointment</p>
-      </section>
+      </section>}
 
       {type !== "cancel" && (
         <>
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { DATABASE_ID, databases, APPOINTMENT_COLLECTION_ID } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -15,7 +17,7 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
 
         return parseStringify(newAppointment);
     } catch (error) {
-        console.log(error);
+        console.error("An error occurred while creating a new appointment:", error);
     }
 }
 
@@ -35,3 +37,77 @@ export const getAppointment = async (appointmentId: string) => {
       );
     }
   };
+
+export const getRecentAppointmentList = async () => {
+    try {
+      const appointments = await databases.listDocuments(
+        DATABASE_ID!,
+        APPOINTMENT_COLLECTION_ID!,
+        [Query.orderDesc("$createdAt")]
+      );
+  
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+  
+      const counts = (appointments.documents as Appointment[]).reduce(
+        (acc, appointment) => {
+          switch (appointment.status) {
+            case "scheduled":
+              acc.scheduledCount++;
+              break;
+            case "pending":
+              acc.pendingCount++;
+              break;
+            case "cancelled":
+              acc.cancelledCount++;
+              break;
+          }
+          return acc;
+        },
+        initialCounts
+      );
+  
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents,
+      };
+  
+      return parseStringify(data);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the recent appointments:",
+        error
+      );
+    }
+};
+
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    // Update appointment to scheduled -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#updateDocument
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if (!updatedAppointment) throw Error;
+
+    // const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!, timeZone).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!, timeZone).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+    // await sendSMSNotification(userId, smsMessage);
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.error("An error occurred while scheduling an appointment:", error);
+  }
+};
